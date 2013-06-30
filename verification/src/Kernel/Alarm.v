a79dfd8f04ffa9fb98920cd200179e0321bf0dbb
Werewolflsp <werewolflsp@gmail.com>
2013-06-30 13:39:08 +0800

    alarm_cstr

--- verification/src/Kernel/Alarm.v
+++ verification/src/Kernel/Alarm.v
@@ -1,14 +1,11 @@
 Set Implicit Arguments.
 
 Record Alarm := mkALM{
-  
   unique_id : nat;
   trigger : nat;
   interval : nat;
   enable : bool
-
 }.
 
-
 (*DO : Alarm construct func, ignore counter alarm data*)
-
+Definition Alarm_cstr (uid t i : nat)(e : bool) : Alarm := mkALM uid t i e.
