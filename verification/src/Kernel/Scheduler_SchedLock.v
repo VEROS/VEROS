4d7c4b8cec3b02e558bc87bf5eea3c0eef0079bc
superwalter <qw04ster@gmail.com>
2013-07-01 15:25:30 +0800

    Add Scheduler_SchedLock

---
+++ verification/src/Kernel/Scheduler_SchedLock.v
@@ -0,0 +1,19 @@
+Set Implicit Arguments.
+
+Record Scheduler_SchedLock := mkss{
+  
+  sched_lock : nat
+
+}.
+
+
+(*DO : Scheduler_SchedLock_cstr*)
+
+(*DO : inc_sched_lock*)
+
+(*DO : zero_sched_lock*)
+
+(*DO : set_sched_lock*)
+
+(*DO : get_sched_lock*)
+
