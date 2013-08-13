10df387dbf55990a31aff1f7e3a54bea2d868539
Werewolflsp <werewolflsp@gmail.com>
2013-08-13 15:38:24 +0800

    added Hardwarethread

---
+++ verification/src/Kernel/HardwareThread.v
@@ -0,0 +1,18 @@
+Set Implicit Arguments.
+
+Definition thread_entry := nat.
+
+Definition ADDRESS := nat.
+
+Record HardwareThread := mkHT {
+  stack_base : ADDRESS;
+  stack_size : nat;
+
+  stack_limit : ADDRESS;
+
+  stack_ptr : ADDRESS;
+  
+  entry_point : thread_entry
+
+  entry_data : ADDRESS;
+}
